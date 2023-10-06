dc17f7d98f636b8c8b49971f4ea33f3c0e3111aa
Younes Bahri <91877321+bahmez@users.noreply.github.com>
2023-10-06 16:01:41 +0200

    [Feat] (GameEngine/RenderEngine) : add Sound

--- RenderEngineBase/Sound/Sound.hpp
+++ RenderEngineBase/Sound/Sound.hpp
@@ -13,17 +13,19 @@ namespace Uniti::Render {
     public:
         enum SoundStatus {
             RESUME,
-            PAUSE
+            PAUSE,
+            UNDEFINED
         };
         bool playSound(const std::string &name, float volume);
         bool playMusic(const std::string &name, float volume, bool loop);
         bool pause(const std::string &name);
         bool reset(const std::string &name);
         bool changeVolume(const std::string &name, float volume);
-        SoundStatus getStatus(const std::string &name) const;
+        SoundStatus getStatus(const std::string &name);
         bool addSound(const Json::Value &value);
-        bool addSound(const std::string &name, const std::string path);
+        bool addSound(const std::string &name, const std::string &path);
+        bool addMusic(const Json::Value &value);
+        bool addMusic(const std::string &name, const std::string &path);
 
     };
-} // Uniti
-// Render
+}
--- RenderEngineSFML/Sound/Sound.hpp
+++ RenderEngineSFML/Sound/Sound.hpp
@@ -4,26 +4,33 @@
 
 #pragma once
 
-
 #include <string>
 #include <json/value.h>
+#include <SFML/Audio.hpp>
+#include <mutex>
 
 namespace Uniti::Render {
     class Sound {
     public:
         enum SoundStatus {
             RESUME,
-            PAUSE
+            PAUSE,
+            UNDEFINED
         };
         bool playSound(const std::string &name, float volume);
         bool playMusic(const std::string &name, float volume, bool loop);
         bool pause(const std::string &name);
         bool reset(const std::string &name);
         bool changeVolume(const std::string &name, float volume);
-        SoundStatus getStatus(const std::string &name) const;
+        SoundStatus getStatus(const std::string &name);
         bool addSound(const Json::Value &value);
-        bool addSound(const std::string &name, const std::string path);
-
+        bool addSound(const std::string &name, const std::string &path);
+        bool addMusic(const Json::Value &value);
+        bool addMusic(const std::string &name, const std::string &path);
+    private:
+        std::map<std::string, sf::SoundBuffer> _soundsBuffer;
+        std::map<std::string, sf::Sound> _sounds;
+        std::map<std::string, std::unique_ptr<sf::Music>> _musics;
+        std::mutex _mutex;
     };
-} // Uniti
-// Render
+}
