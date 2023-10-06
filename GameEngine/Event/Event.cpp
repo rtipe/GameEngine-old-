a503c8b20e1fd80fce75e42af650c75e2ca97952
Younes Bahri <91877321+bahmez@users.noreply.github.com>
2023-10-06 20:27:25 +0200

    [Feat] (GameEngine) : add Event

--- GameEngine/Event/Event.cpp
+++ GameEngine/Event/Event.cpp
@@ -6,5 +6,4 @@
 
 
 namespace Uniti::Game {
-} // Uniti
-// Game
+}
--- GameEngine/Event/Event.hpp
+++ GameEngine/Event/Event.hpp
@@ -4,10 +4,22 @@
 
 #pragma once
 
+#include <string>
+#include <functional>
+#include <map>
+#include <mutex>
+#include "IScript.hpp"
 
 namespace Uniti::Game {
+    using eventFunction = std::function<void()>;
     class Event {
-
+    public:
+        std::size_t addEvent(const std::string &name, eventFunction function);
+        void removeEvent(const std::string &name);
+        template<typename... Args>
+        void emitEvent(const std::string &name, Args... args);
+    private:
+        std::map<std::string, std::vector<eventFunction>> _events;
+        std::mutex _mutex;
     };
-} // Uniti
-// Game
+}
