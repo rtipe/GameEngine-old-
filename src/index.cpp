7fec34880f52ed01e0e4277338c60558e1c5b362
Younes Bahri <91877321+bahmez@users.noreply.github.com>
2023-10-12 15:29:50 +0200

    [Feat] (AnimationScript) : add animation script

--- src/index.cpp
+++ src/index.cpp
@@ -6,12 +6,14 @@
 #include "Uniti.hpp"
 #include "MouseMovement.hpp"
 #include "NetworkTest.hpp"
+#include "Animation.hpp"
 
 int main() {
-    Uniti::Game::Core::initProject("../json/project.json");
+    Uniti::Game::Core::initProject("../../json/project.json");
 
     Uniti::Game::Core::getScriptFactory().addScript<MouseMovement>("MouseMovement");
     Uniti::Game::Core::getScriptFactory().addScript<NetworkTest>("NetworkTest");
+    Uniti::Game::Core::getScriptFactory().addScript<Animation>("Animation");
 
     std::cout << Uniti::Game::Core::getProjectInfo().name << std::endl;
     Uniti::Game::Core::start();
