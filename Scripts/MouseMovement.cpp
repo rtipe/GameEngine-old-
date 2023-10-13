647abbf83fa5ca142273ab11d1d1faf22b2ffd29
Younes Bahri <91877321+bahmez@users.noreply.github.com>
2023-10-13 14:54:31 +0200

    [Feat] (GameEngine) : add MissileHandlere

--- Scripts/MouseMovement.cpp
+++ Scripts/MouseMovement.cpp
@@ -5,41 +5,75 @@
 #include <iostream>
 #include "Uniti.hpp"
 #include "Mouse.hpp"
+#include "Input.hpp"
 #include "MouseMovement.hpp"
 #include "Objects.hpp"
 
 MouseMovement::MouseMovement(Uniti::Game::Object &gameObject) : AScript(gameObject) { }
 
 void MouseMovement::start() {
-    this->getEvent().addEvent("VesselPosition", [&] (const Json::Value &value) {
-        auto data = value["data"];
-        auto position = Uniti::Render::Vector2f(data["position"]);
-        auto id = data["id"].asString();
-        auto copy = Uniti::Game::Utils::Objects::find("exampleVessel");
-        auto vessel = Uniti::Game::Utils::Objects::find(id);
-        if (copy == std::nullopt) return;
-        if (vessel == std::nullopt) {
-            auto newVessel = std::make_unique<Uniti::Game::Object>(copy.value());
-            newVessel->getTransform().getPosition().setX(position.getX());
-            newVessel->getTransform().getPosition().setY(position.getY());
-            newVessel->setName(id);
-            this->getGameObject().getScene().getObjects().add(std::move(newVessel));
-        } else {
-            if (position.getX() != vessel.value().get().getTransform().getPosition().getX() ||
-                position.getY() != vessel.value().get().getTransform().getPosition().getY())
-                vessel.value().get().getMovement().moveTo(position, this->_speed);
+    this->getEvent().addEvent("Vessel", [&] (const Json::Value &value) {
+        for (const auto &nameVessel : value["data"].getMemberNames()) {
+            auto data = value["data"][nameVessel];
+            auto position = Uniti::Render::Vector2f(data["position"]);
+            auto id = data["id"].asString();
+            auto copy = Uniti::Game::Utils::Objects::find("exampleVessel");
+            auto vessel = Uniti::Game::Utils::Objects::find(id);
+            if (copy == std::nullopt) return;
+            if (vessel == std::nullopt) {
+                auto newVessel = std::make_unique<Uniti::Game::Object>(copy.value());
+                newVessel->getTransform().getPosition().setX(position.getX());
+                newVessel->getTransform().getPosition().setY(position.getY());
+                newVessel->setName(id);
+                this->getGameObject().getScene().getObjects().add(std::move(newVessel));
+            } else {
+                if (position.getX() != vessel.value().get().getTransform().getPosition().getX() ||
+                    position.getY() != vessel.value().get().getTransform().getPosition().getY()) {
+                    vessel.value().get().getTransform().getPosition().setX(position.getX());
+                    vessel.value().get().getTransform().getPosition().setY(position.getY());
+                }
+            }
         }
     });
 }
 
 void MouseMovement::update() {
     if (Uniti::Render::Mouse::isMousePressed(Uniti::Render::Mouse::KeyMouse::LEFT)) {
-        Uniti::Render::Vector2f positionMouse = Uniti::Render::Mouse::getPosition(Uniti::Game::Core::getWindow());
-        Json::Value position;
-        position["x"] = positionMouse.getX();
-        position["y"] = positionMouse.getY();
-        Uniti::Game::Core::getNetwork().getServer("game").sendEvent("vesselMovement", position);
+        Uniti::Game::Core::getNetwork().getServer("game").sendEvent("vesselShoot", {});
     }
+    Json::Value position;
+
+    if (!isPressed) {
+        if (Uniti::Render::Input::isPressed(Uniti::Render::Input::Left)) {
+            _normal = {-1, 0};
+            isPressed = true;
+        }
+        if (Uniti::Render::Input::isPressed(Uniti::Render::Input::Right)) {
+            _normal = {1, 0};
+            isPressed = true;
+        }
+        if (Uniti::Render::Input::isPressed(Uniti::Render::Input::Up)) {
+            _normal = {0, -1};
+            isPressed = true;
+        }
+        if (Uniti::Render::Input::isPressed(Uniti::Render::Input::Down)) {
+            _normal = {0, 1};
+            isPressed = true;
+        }
+    } else {
+        if (
+            Uniti::Render::Input::isReleased(Uniti::Render::Input::Down) ||
+            Uniti::Render::Input::isReleased(Uniti::Render::Input::Up) ||
+            Uniti::Render::Input::isReleased(Uniti::Render::Input::Right) ||
+            Uniti::Render::Input::isReleased(Uniti::Render::Input::Left)
+        ) {
+            isPressed = false;
+            _normal = {0, 0};
+        }
+    }
+    position["x"] = _normal.getX();
+    position["y"] = _normal.getY();
+    Uniti::Game::Core::getNetwork().getServer("game").sendEvent("vesselMovement", position);
 }
 
 void MouseMovement::awake(const Json::Value &value) {
--- src/index.cpp
+++ src/index.cpp
@@ -7,7 +7,7 @@
 #include "MouseMovement.hpp"
 #include "Animation.hpp"
 #include "NetworkBase.hpp"
-#include "Missile.hpp"
+#include "MissileHandler.hpp"
 
 int main() {
     Uniti::Game::Core::initProject("../../json/project.json");
@@ -15,7 +15,7 @@ int main() {
     Uniti::Game::Core::getScriptFactory().addScript<MouseMovement>("MouseMovement");
     Uniti::Game::Core::getScriptFactory().addScript<Animation>("Animation");
     Uniti::Game::Core::getScriptFactory().addScript<NetworkBase>("NetworkBase");
-    Uniti::Game::Core::getScriptFactory().addScript<Missile>("Missile");
+    Uniti::Game::Core::getScriptFactory().addScript<MissileHandler>("MissileHandler");
 
     std::cout << Uniti::Game::Core::getProjectInfo().name << std::endl;
     Uniti::Game::Core::start();
